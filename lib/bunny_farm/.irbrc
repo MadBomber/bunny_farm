7ff6d8bf13ad5953a3552071ad905987be56d24b
Dewayne VanHoozer <dvanhoozer@gmail.com>
2015-04-24 14:46:44 -0500

    checkpoint

---
+++ lib/bunny_farm/.irbrc
@@ -0,0 +1,7 @@
+require 'init_bunny.rb'
+$debug = false
+
+BunnyFarm.config do
+  xyz 123
+end
+
--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -2,7 +2,6 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'bunny_farm'
 
 require 'minitest/autorun'
-require 'hashie'
 
 class MyMessageClass < BunnyFarm::Message
   fields :field1, :field2, {field3: [:a, :b]}, {field4: [:c, :d]}
